b0985e79eaf69a30f9a551337faa4fe562e94c0a
lorrinferdinand <lorrin.ferdinand@gmail.com>
2017-09-09 18:40:18 -0700

    Add files via upload

---
+++ PeopleViewer.Test/MainViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PeopleViewer.Test
+{
+    [TestClass]
+    public class MainViewModelTest
+    {
+
+        [TestMethod]
+        public void People_OnFetchData_IsPopulated()
+        {
+            //arrange 
+            var viewModel = new MainViewModel();
+
+            //Act
+            viewModel.FetchData();
+
+            //asserts
+            Assert.IsNotNull(viewModel.People);
+            Assert.AreEqual(2, viewModel.People.Count());
+
+        }
+    }
+}
